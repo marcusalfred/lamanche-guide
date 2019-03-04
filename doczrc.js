60fd3db628add4f81628046ede0dfc491f301120
Marcus Carter <mac987@gmail.com>
2019-03-04 00:36:13 -0600

    updated menu

--- doczrc.js
+++ doczrc.js
@@ -4,15 +4,7 @@ export default {
   menu: [
     'Welcome', // auto ordered menu "Introduction"
     'Transportation',
-    'Food',
-    {
-      name: 'Activities', // manually oredered menu "Guides"
-      // menu: ['Theming'] // oredered items for "Guides",
-      docs: [
-       'NOMA',
-       'WWII Museum',
-       'Preservation Hall'
-     ],
-    },
+    'Where to Eat?',
+    'What to do?'
   ]
 }
